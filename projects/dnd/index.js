02f5a9794f08c6fa0b5b43a15caf32d895dbff0c
Christina Yanovich <christik.lucid@gmail.com>
2022-02-08 09:42:08 +0300

    homework 06 dnd

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,70 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+// перетаскиваемый в данный момент div
+let draggableDiv;
 
-export function createDiv() {}
+// координаты курсора относительно родительского div
+let cursorPosX = 0;
+let cursorPosY = 0;
+
+// функция, возвращающая случайное число в диапазоне от min до max
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// функция, возвращающая случайный цвет
+function getRandomColor() {
+  const min = 0;
+  const max = 255;
+
+  return `rgb(${getRandom(min, max)}, ${getRandom(min, max)}, ${getRandom(min, max)})`;
+}
+
+// функция, задающая элементу случайную позицию
+function setRandomCoords(elem) {
+  const elemWidth = parseInt(elem.style.width);
+  const elemHeight = parseInt(elem.style.height);
+  const winWidth = window.innerWidth;
+  const winHeight = window.innerHeight;
+
+  elem.style.left = getRandom(0, winWidth - elemWidth) + 'px';
+  elem.style.top = getRandom(0, winHeight - elemHeight) + 'px';
+}
+
+document.addEventListener('mousemove', (e) => {
+  if (draggableDiv) {
+    draggableDiv.style.top = e.clientY - cursorPosY + 'px';
+    draggableDiv.style.left = e.clientX - cursorPosX + 'px';
+  }
+});
+
+export function createDiv() {
+  const div = document.createElement('div');
+  const minSize = 30;
+  const maxSize = 100;
+
+  div.classList.add('draggable-div');
+  div.style.width = getRandom(minSize, maxSize) + 'px';
+  div.style.height = getRandom(minSize, maxSize) + 'px';
+  div.style.backgroundColor = getRandomColor();
+  setRandomCoords(div);
+  div.style.cursor = 'move';
+
+  div.addEventListener('mousedown', function (e) {
+    cursorPosX = e.offsetX;
+    cursorPosY = e.offsetY;
+    draggableDiv = div;
+  });
+
+  div.addEventListener('mouseup', function () {
+    draggableDiv = false;
+    cursorPosX = 0;
+    cursorPosY = 0;
+  });
+
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
