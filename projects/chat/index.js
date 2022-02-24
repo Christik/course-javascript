7a0458bfe78c63ae1d9f3bf05f87176651dfe22b
Christina Yanovich <christik.lucid@gmail.com>
2022-02-24 09:41:45 +0300

    adding chat

--- projects/chat/index.js
+++ projects/chat/index.js
@@ -1,42 +1,169 @@
 import './index.html';
+import './css/styles.css';
+import messageTemplate from './templates/message.hbs';
+import noticeTemplate from './templates/notice.hbs';
+import userTemplate from './templates/user.hbs';
 
 const socket = new WebSocket('ws://localhost:9090');
 
-const messageText = document.querySelector('#messageText');
-const sendButton = document.querySelector('#sendButton');
-const messageContainer = document.querySelector('#messages');
+// создаем уникальный id для пользователя
+const uniqueId = new Date().getTime();
+
+// элементы формы входа
+const pageLogin = document.querySelector('.page-login');
+const fieldLogin = document.querySelector('#fieldLogin');
+const btnLogin = document.querySelector('#btnLogin');
+
+// вход в чат
+btnLogin.addEventListener('click', sendLogin);
+fieldLogin.addEventListener('change', sendLogin);
+
+// элементы чата
+const pageChat = document.querySelector('.page-chat');
+const fieldMessage = document.querySelector('#fieldMessage');
+const btnSend = document.querySelector('#btnSend');
+const chatContent = document.querySelector('.chat-content');
+const usersList = document.querySelector('.page-chat__side');
+const fieldCount = document.querySelector('#usersCount');
 
 // получение сообщения от сокет-сервера
 socket.addEventListener('message', function (event) {
-  const data = JSON.parse(event.data).data;
+  const message = JSON.parse(event.data);
 
-  // преобразование в текст
-  let text = '';
-  data.forEach((item) => {
-    text += String.fromCharCode(item);
-  });
+  // если пришла информация о новом пользователе
+  if (message.login) {
+    addNotice(`${message.name} вошел в чат`);
+    addUser(message);
+    // обновляем в разметке количество участников
+    fieldCount.textContent = Object.keys(message.usersList).length;
+  }
 
-  addMessage(text);
+  // если пришла информация о выходе пользователя из чата
+  if (message.logout) {
+    removeUser(message.id);
+    addNotice(`${message.name} вышел из чата`);
+  }
+
+  // если пришло новое сообщение в чат
+  if (message.message) {
+    addMessage(message.message);
+  }
 });
 
+// отправка нового сообщения
+btnSend.addEventListener('click', sendMessage);
+fieldMessage.addEventListener('change', sendMessage);
+
+// соединение с сервером закрыто
 socket.addEventListener('error', function () {
   alert('Соединение закрыто или не может быть открыто');
 });
 
-// добавление сообщения в разметку
-function addMessage(message) {
-  const messageItem = document.createElement('div');
-  messageItem.textContent = message;
-  messageContainer.appendChild(messageItem);
-  messageContainer.scrollTop = messageContainer.scrollHeight;
+// отправка информации о новом пользователе
+function sendLogin(e) {
+  e.preventDefault();
+
+  // введенный ник в форме авторизации
+  const field = fieldLogin.value.trim();
+
+  if (field !== '') {
+    // скрываем окно авторизации и отображаем чат
+    pageLogin.classList.add('is-hidden');
+    pageChat.classList.remove('is-hidden');
+
+    // отправляем ник на сервер
+    const data = {
+      login: true,
+      name: field,
+      id: uniqueId,
+    };
+    socket.send(JSON.stringify(data));
+
+    // очищаем поле с ником
+    fieldLogin.value = '';
+  } else {
+    alert('Введите ник');
+  }
 }
 
-// отправка сообещния
+// добавление уведомления в разметку чата
+function addNotice(text) {
+  chatContent.insertAdjacentHTML(
+    'beforeend',
+    noticeTemplate({
+      text: text,
+    })
+  );
+
+  updateScroll();
+}
+
+// добавление сообщения в разметку чата
+function addMessage(messageObj) {
+  chatContent.insertAdjacentHTML(
+    'beforeend',
+    messageTemplate({
+      isMine: messageObj.id === uniqueId,
+      name: messageObj.name,
+      text: messageObj.text,
+      date: messageObj.date,
+    })
+  );
+
+  updateScroll();
+}
+
+// добавление нового юзера в список юзеров
+function addUser(messageObj) {
+  const list = messageObj.usersList;
+
+  // если это текущий пользователь вошел в чат,
+  // отображаем список всех пользователей,
+  // который пришел с сервера
+  if (messageObj.id === uniqueId) {
+    let fragment = '';
+
+    for (const id in list) {
+      fragment += userTemplate({
+        id: id,
+        name: list[id].name,
+      });
+    }
+
+    usersList.innerHTML = fragment;
+  } else {
+    // если в чат вошел другой пользователь,
+    // то добавляем его в список пользователей
+    usersList.insertAdjacentHTML(
+      'beforeend',
+      userTemplate({
+        id: messageObj.id,
+        name: messageObj.login,
+      })
+    );
+  }
+}
+
+// удаление пользователя из списка
+function removeUser(id) {
+  usersList.querySelector(`[data-id="${id}"]`).remove();
+}
+
+// отправка нового сообщения на сервер
 function sendMessage(e) {
   e.preventDefault();
-  socket.send(messageText.value);
-  messageText.value = '';
+
+  // отправляем сообщение на сервер
+  const data = {
+    message: fieldMessage.value,
+    date: new Date().toTimeString().slice(0, 5),
+  };
+  socket.send(JSON.stringify(data));
+
+  // очищаем поле ввода сообщения
+  fieldMessage.value = '';
 }
 
-sendButton.addEventListener('click', sendMessage);
-messageText.addEventListener('change', sendMessage);
+function updateScroll() {
+  chatContent.scrollTop = chatContent.scrollHeight;
+}
--- projects/chat/server.js
+++ projects/chat/server.js
@@ -1,19 +1,65 @@
 const ws = require('ws');
 const wss = new ws.Server({ port: 9090 });
 
+// список подключенных клиентов
+const usersList = {};
+
 wss.on('connection', function connection(ws) {
   // обработчик события message для приема сообщений
-  ws.on('message', function (message) {
-    // отправляем сообщение всем слушающим клиентам
-    wss.clients.forEach(function (client) {
-      // если у клиента сокет-соединение открыто
-      // отправляем ему сообщение
-      if (client.readyState === ws.OPEN) {
-        client.send(JSON.stringify(message));
-      }
-    });
+  ws.on('message', function message(data) {
+    const message = JSON.parse(data);
+
+    // если залогинился новый пользователь, сохраняем его имя и id,
+    // добавляем его в писок юзеров и рассылаем всем сообщение
+    if (message.login) {
+      ws.id = message.id;
+      ws.userName = message.name;
+
+      usersList[ws.id] = {
+        name: ws.userName,
+      };
+
+      message.usersList = usersList;
+
+      sendAll(message);
+    }
+
+    // если пользователь написал сообщение,
+    // рассылаем это сообщение всем клиентам
+    if (message.message) {
+      sendAll({
+        message: {
+          id: ws.id,
+          name: ws.userName,
+          text: message.message,
+          date: message.date,
+        },
+      });
+    }
   });
 
   // закрытие подключения
-  ws.on('close', function () {});
+  ws.on('close', function () {
+    // удаляем пользователя из списка
+    delete usersList[ws.id];
+
+    // отправляем клиентам информацию о пользователе,
+    // который закрыл подключение
+    sendAll({
+      id: ws.id,
+      name: ws.userName,
+      logout: true,
+    });
+  });
 });
+
+// отправляем сообщение всем слушающим клиентам
+function sendAll(message) {
+  wss.clients.forEach(function (client) {
+    // если у клиента сокет-соединение открыто
+    // отправляем ему сообщение
+    if (client.readyState === ws.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
